bf6923c0ee43d267d94552af698f9ac4b04cf978
mairi tikk <mairi.tikk@gmail.com>
2025-05-06 10:27:06 +0200

    add img

--- shadcn/src/app/page.tsx
+++ shadcn/src/app/page.tsx
@@ -6,6 +6,9 @@ import {
   CardHeader, 
   CardTitle 
 } from '@/components/ui/card'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
 
 interface Recipe {
   title: string,
